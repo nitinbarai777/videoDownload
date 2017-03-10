43ee7755aec3f9dd7b67f9f3bd5751cf94d64b91
chaudhary prakash <chaudharyprakash1992@gmail.com>
2017-03-10 19:40:53 +0530

    added ubauntu video-dl library

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,44 +1,21 @@
-  class HomesController < ApplicationController
+class HomesController < ApplicationController
   require 'viddl-rb'
   require 'open-uri'
-  require 'fileutils'
 
   def index
     if params[:videoUrl].present?
       begin 
-        # video_info = YoutubeDL.download "#{params[:videoUrl]}"
+        # download video resolution wise
+        # @video_info =  `youtube-dl -f 17 -g "#{params[:videoUrl]}"`
+        # download top resolution video
         @video_info =  `youtube-dl -g "#{params[:videoUrl]}"`
         title = `youtube-dl --get-filename "#{params[:videoUrl]}"`
-        video_title = title.split("-")[0]
-        # video_file_title = video_info.information[:requested_formats].first[:url] 
-        # video_file_title = video_info.information[:title] 
-        # @video_url = video_info.information[:url] || video_info.information[:requested_formats].first[:url]
+        video_title = title.strip
 
-        data = open(@video_info) 
-        # video_file_title =  `youtube-dl --get-format "#{params[:videoUrl]}"`
-        send_data data.read, filename: "#{video_title}", disposition: 'attachment', stream: 'true', buffer_size: '4096' 
-
-        # respond_to do |format|
-        #   @url = "/ajax_download?file=#{video_file_title}"
-        #   format.js {render :partial => "downloadFile"}
+        data = open(@video_info)
+        send_data data.read, filename: "#{video_title}", disposition: 'attachment', stream: 'true', buffer_size: '4096'
       rescue Exception => e
       end
     end
   end
-
-  # def ajax_download
-  #   # path_to_video = Rails.root.join('public', 'video.mp4')
-  #    folder = system 'realpath ~/Video'
-  #    p path_to_video1
-  #   send_file path_to_video1,:filename =>  "#{params[:file]}", :range => true, :type => 'video/mp4'
-  #   # File.delete(path_to_video);
-  # end
 end
-
-
-# file-name 
-# youtube-dl -e "https://www.youtube.com/watch?v=2nBJ_FFQuLI"
-
-# --get-duration                   Simulate, quiet but print video length
-# --get-filename                   Simulate, quiet but print output filename
-# --get-format 
