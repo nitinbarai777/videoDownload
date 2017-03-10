6ab768957928ee9a32da2299dbe19098e01b6fb7
chaudhary prakash <chaudharyprakash1992@gmail.com>
2017-03-10 12:50:52 +0530

    change video download command

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,17 +4,19 @@
   require 'fileutils'
 
   def index
-  
     if params[:videoUrl].present?
       begin 
-        video_info = YoutubeDL.download "#{params[:videoUrl]}"
+        # video_info = YoutubeDL.download "#{params[:videoUrl]}"
+        @video_info =  `youtube-dl -g "#{params[:videoUrl]}"`
+        title = `youtube-dl --get-filename "#{params[:videoUrl]}"`
+        video_title = title.split("-")[0]
         # video_file_title = video_info.information[:requested_formats].first[:url] 
-        video_file_title = video_info.information[:title] 
-        @video_url = video_info.information[:url] || video_info.information[:requested_formats].first[:url]
+        # video_file_title = video_info.information[:title] 
+        # @video_url = video_info.information[:url] || video_info.information[:requested_formats].first[:url]
 
-        data = open(@video_url) 
-        send_data data.read, filename: "#{video_file_title}", disposition: 'attachment', stream: 'true', buffer_size: '4096' 
-
+        data = open(@video_info) 
+        # video_file_title =  `youtube-dl --get-format "#{params[:videoUrl]}"`
+        send_data data.read, filename: "#{video_title}", disposition: 'attachment', stream: 'true', buffer_size: '4096' 
 
         # respond_to do |format|
         #   @url = "/ajax_download?file=#{video_file_title}"
@@ -32,3 +34,11 @@
   #   # File.delete(path_to_video);
   # end
 end
+
+
+# file-name 
+# youtube-dl -e "https://www.youtube.com/watch?v=2nBJ_FFQuLI"
+
+# --get-duration                   Simulate, quiet but print video length
+# --get-filename                   Simulate, quiet but print output filename
+# --get-format 
